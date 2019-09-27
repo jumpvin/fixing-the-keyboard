81da681c328caae029c85cf0e00e15e4cb861f70
jumpvin <jumpvince@gmail.com>
2019-09-27 19:37:54 +0200

    finished

--- index.js
+++ index.js
@@ -13,6 +13,11 @@
 
 findBrokenKeys = (str1, str2) => {
   // write your code HERE
+  let broken = [];
+  for (let i = 0; i< str1.length; i++) {
+    if (str1[i] === str2[i]){} else { if(!broken.includes(str1[i])) broken.push(str1[i])}
+  }
+  return broken.length? broken: -1;
 
 
 
